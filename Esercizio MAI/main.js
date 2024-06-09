6f762e44da8e8255cd191ada3a16b289c2e6441a
alessandrofogliada <fogliada.a@gmail.com>
2024-06-09 15:39:07 +0200

    prima prota di email

---
+++ Esercizio MAI/main.js
@@ -0,0 +1,40 @@
+// chiedi all utente la propria E-Mail 
+let container = document.getElementsByClassName(".container")
+
+let emailUtente = document.getElementById("email");
+
+console.log(emailUtente);
+
+// crea tasto per dare l'input 
+let tastoConferma = document.querySelector(".tastoconferma")
+
+tastoConferma.addEventListener ("click" ,
+    function () {
+        let inputValue =emailUtente.value ;
+        console.log(inputValue);
+    }
+)
+
+// crea lista di E-mail idoonee
+
+let esitoEmail = document.querySelector("section");
+
+if (emailUtente === "1") {
+    esitoEmail.style.backgroundColor = "green";
+    esitoEmail.textContent = "idoneo";
+} else if (emailUtente !== "1") {
+    esitoEmail.style.backgroundColor = "red";
+    esitoEmail.textContent = "non idoneo";
+} else {
+    esitoEmail.style.backgroundColor = "blue";
+    esitoEmail.textContent = "1";
+}
+
+tastoConferma.append(emailUtente);
+
+
+
+// confronta la mail inserita dall'utente (if , else)
+
+// dai esito mail all'utente
+
